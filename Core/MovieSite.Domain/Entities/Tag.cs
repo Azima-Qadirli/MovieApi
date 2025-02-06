b35e5fc3fe48bd75c02e73ba3119835ca988d670
Azima <azimaqadirli@gmail.com>
2025-02-06 23:45:34 +0400

    updated

---
+++ Core/MovieSite.Domain/Entities/Tag.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieSite.Domain.Entities
+{
+    public class Tag
+    {
+        public int TagId { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ Infrastructure/MovieSite.Persistence/Context/MovieSiteDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieSite.Domain.Entities;
+
+namespace MovieSite.Persistence.Context
+{
+    public class MovieSiteDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-UM6TF1M;Database=MovieApi;Integrated Security=true;TrustServerCertificate=true;");
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<Cast> Casts { get; set; }
+    }
+}

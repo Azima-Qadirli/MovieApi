fa4ba62c168b3051cd8bea94346887684c8d2f52
Azima <azimaqadirli@gmail.com>
2025-03-15 00:00:33 +0400

    Queries

--- Core/MovieSite.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/GetCastByIdQueryHandler.cs
+++ Core/MovieSite.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/GetCastByIdQueryHandler.cs
@@ -1,6 +1,32 @@
-﻿namespace MovieSite.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
+﻿using MediatR;
+using MovieSite.Application.Features.MediatorDesignPattern.Queries.CastQuery;
+using MovieSite.Application.Features.MediatorDesignPattern.Results.CastResults;
+using MovieSite.Persistence.Context;
+
+namespace MovieSite.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
 {
-    public class GetCastByIdQueryHandler
+    public class GetCastByIdQueryHandler : IRequestHandler<GetCastByIdQuery, GetCastByIdQueryResult>
     {
+        private readonly MovieSiteDbContext _context;
+
+        public GetCastByIdQueryHandler(MovieSiteDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<GetCastByIdQueryResult> Handle(GetCastByIdQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _context.Casts.FindAsync(request.CastId);
+            return new GetCastByIdQueryResult
+            {
+                CastId = values.CastId,
+                Biography = values.Biography,
+                ImageUrl = values.ImageUrl,
+                Name = values.Name,
+                Overview = values.Overview,
+                Surname = values.Surname,
+                Title = values.Title
+            };
+        }
     }
 }
--- Core/MovieSite.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/GetCastQueryHandler.cs
+++ Core/MovieSite.Application/Features/MediatorDesignPattern/Handlers/CastHandlers/GetCastQueryHandler.cs
@@ -1,6 +1,33 @@
-﻿namespace MovieSite.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using MovieSite.Application.Features.MediatorDesignPattern.Queries.CastQuery;
+using MovieSite.Application.Features.MediatorDesignPattern.Results.CastResults;
+using MovieSite.Persistence.Context;
+
+namespace MovieSite.Application.Features.MediatorDesignPattern.Handlers.CastHandlers
 {
-    public class GetCastQueryHandler
+    public class GetCastQueryHandler : IRequestHandler<GetCastQuery, List<GetCastQueryResult>>
     {
+        private readonly MovieSiteDbContext _context;
+
+        public GetCastQueryHandler(MovieSiteDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<GetCastQueryResult>> Handle(GetCastQuery request, CancellationToken cancellationToken)
+        {
+            var values = await _context.Casts.ToListAsync();
+            return values.Select(x => new GetCastQueryResult
+            {
+                CastId = x.CastId,
+                Biography = x.Biography,
+                ImageUrl = x.ImageUrl,
+                Name = x.Name,
+                Surname = x.Surname,
+                Overview = x.Overview,
+                Title = x.Title
+            }).ToList();
+        }
     }
 }

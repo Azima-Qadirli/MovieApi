18d7c8a9a28773ba3e27f5b3765a1a1113aafe2c
Azima <azimaqadirli@gmail.com>
2025-05-11 22:45:46 +0400

    Updated

--- Frontend/MovieSite.WebUI/obj/Debug/net9.0/MovieSite.WebUI.AssemblyInfo.cs
+++ Frontend/MovieSite.WebUI/obj/Debug/net9.0/MovieSite.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieSite.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+474f532c733b82aadbf914ec34d4ed5b34083bea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+909e06b6ad6722feadbf3239fdab412bd8cdbfdd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieSite.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieSite.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
